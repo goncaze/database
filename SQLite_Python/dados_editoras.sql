1e0e3905b145b335cffc517fb37002101e0f0416
goncaze <goncaze@gmail.com>
2024-12-02 13:35:07 -0300

    Dados para inserir

---
+++ SQLite_Python/dados_editoras.sql
@@ -0,0 +1,23 @@
+sql2 = """
+  INSERT INTO editora (cod_editora, telefone1, telefone2, nome, email, consultor_vendas) VALUES
+  (1, '987655432', '912346789', 'Editora Abril', 'contato@editoraabril.com', 'Fernanda Lima'),
+  (2, '987655433', '987655433', 'Editora Globo', 'contato@editoraglobo.com', 'Carlos Souza'),
+  (3, '987655434', '912346780', 'Editora Moderna', 'contato@editoramoderna.com', 'Paula Mendes'),
+  (4, '987655435', '912346781', 'Editora Saraiva', 'contato@saraiva.com', 'Marcos Silva'),
+  (5, '987655436', '912346782', 'Editora Atlas', 'contato@editoraatlas.com', 'Luciana Costa'),
+  (6, '987655437', '912346783', 'Editora FGV', 'contato@fgv.com', 'Fábio Almeida'),
+  (7, '987655438', '987655438', 'Editora Senac', 'contato@senac.com', 'Carla Menezes'),
+  (8, '987655439', '912346784', 'Editora Blucher', 'contato@blucher.com', 'Rafael Dias'),
+  (9, '987655440', '912346785', 'Editora Loyola', 'contato@loyola.com', 'Amanda Santos'),
+  (10, '987655441', '912346786', 'Editora Nobel', 'contato@nobel.com', 'Eduardo Pereira'),
+  (11, '987655442', '912346787', 'Editora Juspodivm', 'contato@juspodivm.com', 'Thiago Castro'),
+  (12, '987655443', '912346788', 'Editora Zahar', 'contato@editorazahar.com', 'Mariana Lopes'),
+  (13, '987655444', '912346789', 'Editora Contexto', 'contato@editoracontexto.com', 'Rodrigo Tavares'),
+  (14, '+55 (011) 3022-0379', '(031) 4472-1387', 'Farias', 'vsilva@nogueira.com', 'Enzo Gabriel Nunes'),
+  (15, '(031) 7247 7569', '31 8038 8395', 'da Rocha', 'catarina03@da.com', 'Dra. Emanuella Nunes'),
+  (16, '61 0432-2803', '+55 84 3182-7301', 'Rezende Teixeira - EI', 'eduarda92@carvalho.com', 'Dra. Vitória Carvalho'),
+  (17, '21 7666 0402', '(071) 9804-2817', 'Correia', 'kamillycunha@pinto.br', 'Joana Peixoto'),
+  (18, '(051) 9819-0188', '(084) 9760-9967', 'Rodrigues - ME', 'julia45@da.org', 'Maria Alice Pinto'),
+  (19, '21 8541-7653', '+55 84 5377-5861', 'Campos', 'mouraisadora@costela.net', 'Lucas Gabriel Costela'),
+  (20, '+55 (041) 1272-5198', '(081) 8758 5903', 'Teixeira', 'camila52@rodrigues.br', 'Miguel Fernandes');
+"""
